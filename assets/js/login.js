159f3c9d7a8401aa70e783caaa56b9108535efcd
AlaaAlhilly <alaa.naji.us@gmail.com>
2018-11-14 12:35:18 -0600

    fix sign in bug

--- assets/js/login.js
+++ assets/js/login.js
@@ -36,21 +36,16 @@ function onGoogle() {
 
         .then(result => {
             const user = result.user;
-            if (document.cookie['userEmail'] && document.cookie['userEmail'] == user.email) {
-                window.location.replace("schedule.html");
-
-            } else {
-                createUser(user);
-            }
+            createUser(user);
         })
         .catch(console.log)
 
 
 }
-function createUser(user) {
+async function createUser(user) {
     var admin = false;
-    
-    database.ref('/users').orderByChild('email').equalTo(user.email)
+    let a = await function(){
+        database.ref('/users').orderByChild('email').equalTo(user.email)
         .once('value').then(function (snapshot) {
             snapshot.forEach(function (childSnapshot) {
                 console.log(childSnapshot.val().email);
@@ -58,20 +53,20 @@ function createUser(user) {
                 document.cookie = 'userEmail='+ user.email;
                 window.location.replace("schedule.html");
                 return;
-            }).then(function(){
-                if(emailNotFound){
-                    document.cookie = 'userEmail='+ user.email;
-                    database.ref('/users').push({
-                        email: user.email,
-                        admin: admin
-                    });
-                    window.location.replace("schedule.html");
-                }
             });
                 
+        },function(err){
+            alert(err);
         });
-        
-    
+    }
+        if(emailNotFound){
+            document.cookie = 'userEmail='+ user.email;
+            database.ref('/users').push({
+                email: user.email,
+                admin: admin
+            });
+            window.location.replace("schedule.html");
+        }
 }
 function onGit() {
     const provider = new firebase.auth.GithubAuthProvider();
@@ -80,12 +75,7 @@ function onGit() {
 
     .then(result => {
         const user = result.user;
-        if (document.cookie['userEmail'] && document.cookie['userEmail'] == user.email) {
-            window.location.replace("schedule.html");
-
-        } else {
-            createUser(user);
-        }
+        createUser(user);
     })
     .catch(console.log)
 
