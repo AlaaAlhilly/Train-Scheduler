d7df5a424147d8d695715e32bc2d38ec853bb2e4
AlaaAlhilly <alaa.naji.us@gmail.com>
2018-11-14 12:12:09 -0600

    add redirect

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,20 @@
+$(document).ready(function(){
 
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+var loggedEmail = readCookie('userEmail');
+if (loggedEmail != null) {
+    window.location.replace("schedule.html");
+}
+});
 // Initialize Firebase
 
 var config = {
